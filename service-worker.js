3ccd8e7c7d7d73e4f81282669d7ea1fcb539ad91
tomriddle7 <tomriddle7@naver.com>
2025-04-12 11:05:18 +0900

    Update project

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'conceptproof';
+var cacheName = 'psychologer';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
